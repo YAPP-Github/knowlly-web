f337863e1a3c67ef506603f03e81e67cb3413c5e
useonglee <useong0830@naver.com>
2022-05-22 23:33:50 +0900

    #3 feat: button 공통 컴포넌트 추가

--- src/components/Common/Button/Button.tsx
+++ src/components/Common/Button/Button.tsx
@@ -1,7 +1,21 @@
+import { PropsWithChildren } from 'react';
 import * as Styled from './ButtonStyle';
 
-const Button = () => {
-  return <Styled.ButtonStyle>Button</Styled.ButtonStyle>;
+interface IButtonProps {
+  variant: 'contained' | 'enabled' | 'outlined';
+  size?: 'x-small' | 'small' | 'middle' | 'big';
+  type: 'button' | 'submit' | 'reset';
+  _onClick?: () => void;
+}
+
+const Button = (props: PropsWithChildren<IButtonProps>) => {
+  const { _onClick, children, ...reset } = props;
+
+  return (
+    <Styled.ButtonStyle {...reset} onClick={_onClick}>
+      {children}
+    </Styled.ButtonStyle>
+  );
 };
 
 export default Button;
--- src/components/Common/Button/ButtonStyle.ts
+++ src/components/Common/Button/ButtonStyle.ts
@@ -1,3 +1,60 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const ButtonStyle = styled.button``;
+export const ButtonStyle = styled.button<{ variant: string; size?: string }>`
+  ${({ variant, size }) => {
+    return css`
+      ${handleVariant(variant)}
+      ${handleSize(size)}
+      font-size: 1.4rem;
+      border-radius: 10px;
+    `;
+  }}
+`;
+
+const handleVariant = (variant: string) => {
+  switch (variant) {
+    case 'contained':
+      return css`
+        color: #ffffff;
+        background-color: #ff8e4d;
+      `;
+    case 'enabled':
+      return css`
+        color: #8f8f8f;
+        background-color: #ffd5ae;
+      `;
+    case 'outlined':
+      return css`
+        color: #ff7a00;
+        border: 1px solid #ff7a00;
+        background-color: #ffffff;
+      `;
+    // no default
+  }
+};
+
+const handleSize = (size: string | undefined) => {
+  switch (size) {
+    case 'x-small':
+      return css`
+        width: 12.9rem;
+        height: 4rem;
+      `;
+    case 'small':
+      return css`
+        width: 15.8rem;
+        height: 5.6rem;
+      `;
+    case 'middle':
+      return css`
+        width: 31.2rem;
+        height: 4.4rem;
+      `;
+    case 'big':
+      return css`
+        width: 32.8rem;
+        height: 5.6rem;
+      `;
+    // no default
+  }
+};
