14384ec2d3c28d5734c0ab2bd921ef6344c3ce81
useonglee <useong0830@naver.com>
2022-05-22 23:37:27 +0900

    #3 feat: button 공통 컴포넌트 UI 테스트 코드

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,20 @@
 import type { NextPage } from 'next';
+import { Button, FabButton, TextMoreButton } from '@components/Common';
 
 const Home: NextPage = () => {
-  return <div></div>;
+  const handleTestButton = () => {
+    console.log('check');
+  };
+
+  return (
+    <>
+      <Button variant="contained" type="button" size="big" _onClick={handleTestButton}>
+        test button
+      </Button>
+      <TextMoreButton _onClick={handleTestButton} />
+      <FabButton _onClick={handleTestButton} />
+    </>
+  );
 };
 
 export default Home;
