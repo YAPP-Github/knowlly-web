fa9460bfa453897bf581f43738f77dbc56cb5f14
useonglee <useong0830@naver.com>
2022-05-29 22:43:13 +0900

    #11 feat: button storybook 추가

---
+++ src/components/Common/Button/Button.stories.tsx
@@ -0,0 +1,15 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import Button from './Button';
+
+export default {
+  component: Button,
+  title: 'Common/Button',
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args}>매칭 신청하기</Button>;
+
+export const Contained = Template.bind({});
+Contained.args = {
+  size: 'x-small',
+};
