ab5bfcbec4ebe9c26c21818ccd3f0bd918377b48
TaeeunKim <xodms0309@naver.com>
2022-05-29 20:38:57 +0900

    #9 feat: contained badge 뼈대 생성

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,12 @@
 import type { NextPage } from 'next';
-import { Button, FabButton, TextMoreButton, Input, TextArea } from '@components/Common';
+import {
+  Button,
+  FabButton,
+  TextMoreButton,
+  Input,
+  TextArea,
+  ContainedBadge,
+} from '@components/Common';
 
 const Home: NextPage = () => {
   const handleTestButton = () => {
@@ -21,6 +28,7 @@ const Home: NextPage = () => {
       <FabButton _onClick={handleTestButton} />
       <Input placeholder="default" maxLength={10} />
       <TextArea placeholder="default" maxLength={300} />
+      <ContainedBadge type={'category'}>외국/언어</ContainedBadge>
     </>
   );
 };
