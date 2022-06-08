41f968612ce44e7c3ca4c7067d4555fd8ca6881f
useonglee <useong0830@naver.com>
2022-06-08 18:29:30 +0900

    #4 feat: SUIT font 글로벌 환경 설정

---
+++ src/styles/font.ts
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from 'styled-components';
+
+const Font = createGlobalStyle`
+@font-face {
+  font-family: 'SUIT-Regular';
+  font-weight: 400;
+  src: url('./assets/fonts/SUIT-Regular.woff2') format('woff2');
+}
+@font-face {
+  font-family: 'SUIT-Medium';
+  font-weight: 500;
+  src: url('./assets/fonts/SUIT-Medium.woff2') format('woff2');
+}
+@font-face {
+  font-family: 'SUIT-SemiBold';
+  font-weight: 600;
+  src: url('./assets/fonts/SUIT-SemiBold.woff2') format('woff2');
+}
+@font-face {
+  font-family: 'SUIT-Bold';
+  font-weight: 700;
+  src: url('./assets/fonts/SUIT-Bold.woff2') format('woff2');
+}
+`;
+
+export default Font;
