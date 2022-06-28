51b30ffcb481083ddd7484b5b3aa19cd0b1cff4a
useonglee <useong0830@naver.com>
2022-06-28 23:27:00 +0900

    #25 feat: 메인페이지 UI 작업

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,77 +1,23 @@
+import { useState } from 'react';
 import type { NextPage } from 'next';
-import {
-  Button,
-  FabButton,
-  TextMoreButton,
-  Input,
-  TextArea,
-  ContainedBadge,
-  OutlinedBadge,
-  SvgIcon,
-  Typograpy,
-} from '@components/Common';
+import { SearchBar, Typograpy } from '@components/Common';
+import { Layout, Section } from '@components/Layout';
+import { Category, LectureList } from '@components/Home';
 
 const Home: NextPage = () => {
-  const handleTestButton = () => {
-    console.log('check');
-  };
+  const [searchValue, setSearchValue] = useState<string>('');
 
   return (
-    <>
-      <Button
-        variant="contained"
-        type="button"
-        size="xx-small"
-        disabled={true}
-        _onClick={handleTestButton}
-      >
-        test button
-      </Button>
-      <Button>Default</Button>
-      <TextMoreButton _onClick={handleTestButton} />
-      <FabButton _onClick={handleTestButton} />
-      <Input placeholder="default" maxLength={10} />
-      <TextArea placeholder="default" maxLength={300} />
-      <SvgIcon type="checkbox-checked" />
-      <SvgIcon type="checkbox-default" />
-      <SvgIcon type="clear" size={26} />
-      <SvgIcon type="k_class" size={16} />
-      <SvgIcon type="review" size={16} />
-      <SvgIcon type="previous" />
-      <SvgIcon type="indicator-1-active" />
-      <SvgIcon type="indicator-1-inactive" />
-      <SvgIcon type="indicator-2-active" />
-      <SvgIcon type="indicator-2-inactive" />
-      <SvgIcon type="list-checkbox-checked" />
-      <SvgIcon type="list-checkbox-default" />
-      <SvgIcon type="search" />
-      <SvgIcon type="chevron-down-fill" />
-      <SvgIcon type="chevron-down" />
-      <SvgIcon type="chevron-up" />
-      <SvgIcon type="chevron-right" />
-      <SvgIcon type="category-1" />
-      <SvgIcon type="category-2" />
-      <SvgIcon type="category-3" />
-      <p>글로벌 폰트 테스트</p>
-      <Typograpy variant="headline-1" textColor="default">
-        헤더-1
-      </Typograpy>
-      <Typograpy variant="subtitle-3" textColor="primary">
-        서브헤더-3
-      </Typograpy>
-      <Typograpy variant="subtitle-3" textColor="indigo">
-        서브헤더-3
-      </Typograpy>
-      <Typograpy variant="body-2" textColor="warning">
-        바디-2
-      </Typograpy>
-      <Typograpy variant="body-2" textColor="gray8F">
-        바디-2
-      </Typograpy>
-      <Button size="x-small" type="button">
-        <Typograpy variant="button-1">버튼-1</Typograpy>
-      </Button>
-    </>
+    <Layout spacing>
+      <SearchBar placeholder="어떤 클래스를 찾고 싶으신가요?" setSearchValue={setSearchValue} />
+      <Section start={2}>
+        <Category />
+      </Section>
+      <Section start={4}>
+        <Typograpy variant="headline-4">최근 등록된 클래스</Typograpy>
+        <LectureList />
+      </Section>
+    </Layout>
   );
 };
 
