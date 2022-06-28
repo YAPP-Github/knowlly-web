b1f6b31039bfbab42bad33224b3ca284ddaa6c8e
useonglee <useong0830@naver.com>
2022-06-28 23:26:26 +0900

    #25 feat: 메인페이지 최근 등록된 강의 클래스 리스트 UI 작업

---
+++ src/components/Home/LectureList/LectureList.tsx
@@ -0,0 +1,12 @@
+import { LectureCard } from '@components/Home';
+import * as Styled from './LectureListStyle';
+
+const LectureList = () => {
+  return (
+    <Styled.LectureListContainer>
+      <LectureCard />
+    </Styled.LectureListContainer>
+  );
+};
+
+export default LectureList;
