c1846719df95e6309dd62a5e0465add3d1e5a3ad
useonglee <useong0830@naver.com>
2022-06-30 23:14:45 +0900

    #48 feat: env 환경 변수 설정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+    TEST_TOKEN: process.env.NEXT_PUBLIC_TEST_TOKEN,
+  },
 };
 
 module.exports = nextConfig;
