65e4f480fff15001febc554bb29695da7a56fa0d
useonglee <useong0830@naver.com>
2022-07-05 23:48:58 +0900

    #52 feat: 검색 모달창 핸들링 이벤트 함수 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,33 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import type { NextPage } from 'next';
 import { SearchBar, Typograpy } from '@components/Common';
 import { PageLayout, Section } from '@components/Common/Layout';
-import { Category, LectureList } from '@components/Home';
+import { Category, LectureList, SearchModal } from '@components/Home';
+import useLectureInfo from '@hooks/home/useLectureInfo';
 
 const Home: NextPage = () => {
-  const [searchValue, setSearchValue] = useState<string>('');
+  const [isShowSearchModal, setIsShowSearchModal] = useState<boolean>(false);
+
+  const lectureInfoList = useLectureInfo();
+
+  const handleSearchModalDisplay = useCallback(() => {
+    setIsShowSearchModal((prev) => !prev);
+  }, [isShowSearchModal]);
 
   return (
-    <PageLayout isSpacing>
-      <SearchBar placeholder="어떤 클래스를 찾고 싶으신가요?" setSearchValue={setSearchValue} />
+    <PageLayout isSpacing start={1} end={4}>
+      <SearchBar
+        placeholder="어떤 클래스를 찾고 싶으신가요?"
+        _onClickSeachBar={handleSearchModalDisplay}
+      />
       <Section start={2}>
         <Category />
       </Section>
       <Section start={4}>
         <Typograpy variant="headline-4">최근 등록된 클래스</Typograpy>
-        <LectureList />
+        <LectureList lectureInfoList={lectureInfoList} />
       </Section>
+      {isShowSearchModal && <SearchModal handleSearchModalDisplay={handleSearchModalDisplay} />}
     </PageLayout>
   );
 };
