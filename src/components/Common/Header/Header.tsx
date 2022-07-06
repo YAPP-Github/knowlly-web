021ac2a4835cb387b2bdbf9b1d56e2335a893486
useonglee <useong0830@naver.com>
2022-07-06 22:42:36 +0900

    #52 feat: Header 컴포넌트 hasLine props 추가

--- src/components/Common/Header/Header.tsx
+++ src/components/Common/Header/Header.tsx
@@ -4,16 +4,18 @@ import * as Styled from './HeaderStyle';
 
 interface IHeaderProps {
   hasBackButton?: boolean;
+  hasLine?: boolean;
   _onClickBackButton?: () => void;
 }
 
 const Header = ({
   hasBackButton,
+  hasLine,
   _onClickBackButton,
   children,
 }: PropsWithChildren<IHeaderProps>) => {
   return (
-    <Styled.HeaderStyle>
+    <Styled.HeaderStyle hasLine={hasLine}>
       {hasBackButton && (
         <Styled.BackButton onClick={_onClickBackButton}>
           <SvgIcon type="previous" />
