95b7b643b26dc05fda3f8a838caec6faa3dcbf7d
TaeeunKim <xodms0309@naver.com>
2022-07-09 18:38:37 +0900

    #78: feat: 리뷰 더보기 기능 구현

--- src/components/Profile/LectureReview/LectureReview.tsx
+++ src/components/Profile/LectureReview/LectureReview.tsx
@@ -6,40 +6,39 @@ import { useRouter } from 'next/router';
 import useCoachReview from '@hooks/profile/useCoachReview';
 
 const LectureReview = () => {
-  const [endIdx, setEndIdx] = useState(0);
-
-  const toggleReviewList = () => {
-    setEndIdx(endIdx + 2);
-  };
-
   const router = useRouter();
   const userId = Number(router.query.id);
 
-  const reviews = useCoachReview(userId).data;
+  const [endIdx, setEndIdx] = useState(0);
+  const totalReviews = useCoachReview(userId).data;
+  const slicedReviews = useCoachReview(userId).data.slice(0, endIdx + 4);
+  const totalReviewCount = totalReviews.length;
 
-  const totalReviewCount = reviews.length;
-  console.log(totalReviewCount);
+  const toggleReviewList = () => {
+    setEndIdx(endIdx + 4);
+  };
 
   return (
     <Styled.LectureReviewContainer>
       <Typograpy variant="subtitle-1">플레이어 후기</Typograpy>
-      {!totalReviewCount && (
+      {totalReviewCount ? (
+        slicedReviews.map((review, id) => <Review key={id} coachReview={review} />)
+      ) : (
         <Styled.NoReview>
           <Typograpy variant="body-1" textColor="gray8F">
             플레이어 후기가 아직 없어요.
           </Typograpy>
         </Styled.NoReview>
       )}
-      {reviews.map((review) => (
-        <Review key={review.reviewee.id} coachReview={review} />
-      ))}
 
-      <Styled.ShowMore onClick={toggleReviewList}>
-        <Typograpy variant="subtitle-4" textColor="gray6B">
-          더보기
-        </Typograpy>
-        <SvgIcon type="chevron-down" />
-      </Styled.ShowMore>
+      {totalReviewCount > endIdx && (
+        <Styled.ShowMore onClick={toggleReviewList}>
+          <Typograpy variant="subtitle-4" textColor="gray6B">
+            더보기
+          </Typograpy>
+          <SvgIcon type="chevron-down" />
+        </Styled.ShowMore>
+      )}
     </Styled.LectureReviewContainer>
   );
 };
