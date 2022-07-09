cdaa3b3a9397c3a4fafdc90cbb969e0f17b72c99
useonglee <useong0830@naver.com>
2022-07-09 21:44:36 +0900

    #80 feat: lecture-category 페이지 테스트 코드 작성

---
+++ src/__tests__/category/LectureCategory.test.tsx
@@ -0,0 +1,40 @@
+import LectureCategoryPage from '@pages/lecture-category/[id]';
+import { screen, within } from '@testing-library/react';
+import { renderWithQueryClient } from '@test-utils';
+
+jest.mock('next/router', () => ({
+  useRouter() {
+    return {
+      route: '/lecture-category',
+      pathname: '',
+      query: { id: '3' },
+      asPath: '',
+      push: jest.fn(),
+      events: {
+        on: jest.fn(),
+        off: jest.fn(),
+      },
+      beforePopState: jest.fn(() => null),
+      prefetch: jest.fn(() => null),
+    };
+  },
+}));
+
+test('클래스 개발 카테고리 타이틀', () => {
+  renderWithQueryClient(<LectureCategoryPage />);
+
+  const categoryTitle = screen.getByRole('heading', {
+    name: '개발',
+  });
+
+  expect(categoryTitle).toBeInTheDocument();
+});
+
+test('클래스 개발 카테고리 목록 스켈레톤 로딩 UI', () => {
+  renderWithQueryClient(<LectureCategoryPage />);
+
+  const categoryList = screen.getByRole('list');
+  const categoryItems = within(categoryList).getAllByRole('listitem');
+
+  expect(categoryItems).toHaveLength(10);
+});
