312837bcf6bda7651d5251bb229309fcd768a59b
useonglee <useong0830@naver.com>
2022-07-11 20:14:57 +0900

    #81 feat: 운영클래스 매칭신청서 데이터 query hook 함수 추가

---
+++ src/hooks/coachLecture/useMatchingForm.ts
@@ -0,0 +1,18 @@
+import { IPlayerUserForm } from '@/types/coachLecture';
+import api from '@api';
+import queryKeys from '@react-query/keys';
+import { useQuery } from 'react-query';
+
+interface IUseMatchingForm {
+  matchingFormData: IPlayerUserForm;
+}
+
+const useMatchingForm = (formId: string): IUseMatchingForm => {
+  const { data: matchingFormData } = useQuery([queryKeys.coachLecture, formId], () =>
+    api.fetchCoachLectureForm(formId)
+  );
+
+  return { matchingFormData: matchingFormData.data };
+};
+
+export default useMatchingForm;
