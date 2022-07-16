e7d6ebfde28d886d0920cf469a606631feb2ac12
useonglee <useong0830@naver.com>
2022-07-16 16:38:39 +0900

    #101 feat: header 컴포넌트 뒤로가기 버튼 이벤트 함수에서 Link로 변경

--- src/components/Common/Header/Header.tsx
+++ src/components/Common/Header/Header.tsx
@@ -1,25 +1,22 @@
 import { PropsWithChildren } from 'react';
+import Link from 'next/link';
 import SvgIcon from '../Svg/SvgIcon';
 import * as Styled from './HeaderStyle';
 
 interface IHeaderProps {
   hasBackButton?: boolean;
   hasLine?: boolean;
-  _onBackButtonClick?: () => void;
 }
 
-const Header = ({
-  hasBackButton,
-  hasLine,
-  _onBackButtonClick,
-  children,
-}: PropsWithChildren<IHeaderProps>) => {
+const Header = ({ hasBackButton, hasLine, children }: PropsWithChildren<IHeaderProps>) => {
   return (
     <Styled.HeaderStyle hasLine={hasLine}>
       {hasBackButton && (
-        <Styled.BackButton onClick={_onBackButtonClick}>
-          <SvgIcon type="previous" />
-        </Styled.BackButton>
+        <Link href="/">
+          <Styled.BackButton>
+            <SvgIcon type="previous" />
+          </Styled.BackButton>
+        </Link>
       )}
       {children && <Styled.ChildrenWapper>{children}</Styled.ChildrenWapper>}
     </Styled.HeaderStyle>
