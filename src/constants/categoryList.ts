35dd3fdebca49bc7b03b68c39089a053a2499de2
useonglee <useong0830@naver.com>
2022-07-17 03:44:24 +0900

    #95 feat: API 변경에 따라 id -> name으로 수정

--- src/constants/categoryList.ts
+++ src/constants/categoryList.ts
@@ -1,41 +1,41 @@
 import { SvgIconType } from '@components/Common/Svg/SvgIcon';
 
 interface ICategoryList {
-  id: number;
-  icon: SvgIconType;
   name: string;
+  icon: SvgIconType;
+  title: string;
 }
 
 const CATEGORY_LIST: ICategoryList[] = [
   {
-    id: 1,
+    name: 'PM',
     icon: 'category-service-planning',
-    name: '기획 / PM',
+    title: '기획 / PM',
   },
   {
-    id: 2,
+    name: 'DESIGN',
     icon: 'category-design',
-    name: '디자인',
+    title: '디자인',
   },
   {
-    id: 3,
+    name: 'DEVELOP',
     icon: 'category-develop',
-    name: '개발',
+    title: '개발',
   },
   {
-    id: 4,
+    name: 'MARKETING',
     icon: 'category-marketing',
-    name: '마케팅',
+    title: '마케팅',
   },
   {
-    id: 5,
+    name: 'LANGUAGE',
     icon: 'category-language',
-    name: '외국어',
+    title: '외국어',
   },
   {
-    id: 6,
+    name: 'ETC',
     icon: 'category-etc',
-    name: '기타',
+    title: '기타',
   },
 ];
 
