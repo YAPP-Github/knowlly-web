7095bff5bacb86d1046369a0e9405c6f0fafae01
useonglee <useong0830@naver.com>
2022-07-17 00:08:36 +0900

    #91 feat: storybook alias path 목록에 components 추가

--- .storybook/main.js
+++ .storybook/main.js
@@ -13,6 +13,7 @@ module.exports = {
   webpackFinal: async (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
+      '@components': path.resolve(__dirname, '../src/components'),
       '@styles': path.resolve(__dirname, '../src/styles'),
     };
     return config;
