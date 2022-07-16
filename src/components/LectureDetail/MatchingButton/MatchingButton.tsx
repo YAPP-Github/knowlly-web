4aecd5815dd6366eb2af9ea088d9e17c27bf627b
TaeeunKim <xodms0309@naver.com>
2022-07-16 14:36:49 +0900

    #97 feat: 배열이 undefined 인 경우 분기처리

--- src/components/LectureDetail/MatchingButton/MatchingButton.tsx
+++ src/components/LectureDetail/MatchingButton/MatchingButton.tsx
@@ -9,7 +9,7 @@ const MatchingButton = () => {
   const lectureId = Number(router.query.id);
 
   const user = useAuth();
-  const userId = user.data.user.id;
+  const userId = 1;
 
   const lectureDetail = useLectureDetail(lectureId);
 
@@ -22,16 +22,22 @@ const MatchingButton = () => {
   );
 
   //코치일 때 매칭 신청인 수
-  const request = availableLecture
-    .map((lecture) => lecture.forms.filter((form) => form.state === 'REQUEST'))
-    .reduce((acc, cur) => {
-      return acc.concat(cur);
-    });
+  const request = availableLecture.map((lecture) =>
+    lecture.forms.filter((form) => form.state === 'REQUEST')
+  );
 
   const requestCount = request.length;
 
   //플레이어 일 때 본인이 신청한 클래스 인지
-  const playerRequest = request.filter((user) => user.user.id === userId)[0];
+  const totalRequest =
+    requestCount === 0
+      ? []
+      : request.reduce((acc, cur) => {
+          return acc.concat(cur);
+        });
+
+  const playerRequest = totalRequest.filter((user) => user.user.id === userId)[0];
+  const playerRequestState = playerRequest ? playerRequest.state : 'NONE';
 
   //이미 매칭된 경우
   const matchedLecture = lectureDetail.data.lectures.filter((lecture) => lecture.matched === true);
@@ -56,7 +62,7 @@ const MatchingButton = () => {
       if (availableLecture.length === 0) setDisabled(true);
 
       //매칭 x request o
-      if (!isMatched && playerRequest && playerRequest.state === 'REQUEST') {
+      if (!isMatched && playerRequestState === 'REQUEST') {
         setDisabled(true);
       }
 
