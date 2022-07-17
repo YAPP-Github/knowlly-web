253bf8a773877cfe7c028aae881f5965dece161d
useonglee <useong0830@naver.com>
2022-07-17 23:19:24 +0900

    #102 feat: 운영클래스 매칭신청서 플레이어가 보낸 일정 API 데이터 적용

--- src/components/CoachLecture/PlayerSchedule/PlayerSchedule.tsx
+++ src/components/CoachLecture/PlayerSchedule/PlayerSchedule.tsx
@@ -1,19 +1,31 @@
+import { useMemo } from 'react';
 import { Typograpy } from '@components/Common';
-import { formatDate } from '@utils';
+import { formatDate, formatTime, getLectureTime } from '@utils';
 import * as Styled from './PlayerScheduleStyle';
 
 interface IPlayerScheduleProps {
-  schedules: string;
+  startAt: string;
+  endAt: string;
 }
 
-const PlayerSchedule = ({ schedules }: IPlayerScheduleProps) => {
+const PlayerSchedule = ({ startAt, endAt }: IPlayerScheduleProps) => {
+  const scheduleSentByPlayer = useMemo(() => {
+    const startTime = formatTime(startAt);
+    const lectureTime = getLectureTime(startAt, endAt);
+
+    return `${startTime} (${lectureTime}시간 수업)`;
+  }, [startAt, endAt]);
+
   return (
     <>
       <Typograpy variant="body-2" textColor="gray8F">
         해당 일정에 클래스를 여는 것이 가능하다면 수락해주세요.
       </Typograpy>
       <Styled.PlayerUserSchedule>
-        <Typograpy variant="subtitle-4">{formatDate(schedules)}</Typograpy>
+        <Typograpy variant="subtitle-4">{formatDate(startAt)}</Typograpy>
+        <Typograpy variant="body-2" textColor="gray6B">
+          {scheduleSentByPlayer}
+        </Typograpy>
       </Styled.PlayerUserSchedule>
     </>
   );
