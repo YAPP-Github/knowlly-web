42afc38a3d11922cb588cd20b5ecf030fa21a3e0
useonglee <useong0830@naver.com>
2022-07-20 19:14:41 +0900

    #110 feat: 클래스 카드에 link 제거 및 안드로이드 브릿지 연결

--- src/components/Home/LectureCard/LectureCard.tsx
+++ src/components/Home/LectureCard/LectureCard.tsx
@@ -1,4 +1,3 @@
-import Link from 'next/link';
 import { ContainedBadge, Image } from '@components/Common';
 import { ILectureInfoData } from '@/types/lectureInfo';
 import * as Styled from './LectureCardStyle';
@@ -8,35 +7,35 @@ interface ILectureCardProps {
 }
 
 const LectureCard = ({ lectureData }: ILectureCardProps) => {
+  const handleMoveToLectureDetailPageClick = () => {
+    window.Android?.navigateToLecture({ lectureInfoId: lectureData.id });
+  };
+
   return (
-    <li>
-      <Link href={`/lecture-detail/${lectureData.id}`}>
-        <Styled.LectureCardContainer>
-          <Styled.LectureImageWrapper>
-            <Image
-              type="lecture-list"
-              src={lectureData.coach.user.userImgUrl}
-              alt={lectureData.introduce}
-            />
-          </Styled.LectureImageWrapper>
-          <Styled.LectureInfoWrapper>
-            <Styled.LectureIntroduce variant="subtitle-2">
-              {lectureData.introduce}
-            </Styled.LectureIntroduce>
-            <Styled.LectureCoachName variant="body-2" textColor="gray8F">
-              {lectureData.coach.user.username}
-            </Styled.LectureCoachName>
-            <Styled.LectureTags>
-              {lectureData.tags?.map((tag) => (
-                <ContainedBadge key={tag.id} type="category">
-                  {tag.content}
-                </ContainedBadge>
-              ))}
-            </Styled.LectureTags>
-          </Styled.LectureInfoWrapper>
-        </Styled.LectureCardContainer>
-      </Link>
-    </li>
+    <Styled.LectureCardContainer onClick={handleMoveToLectureDetailPageClick}>
+      <Styled.LectureImageWrapper>
+        <Image
+          type="lecture-list"
+          src={lectureData.coach.user.userImgUrl}
+          alt={lectureData.introduce}
+        />
+      </Styled.LectureImageWrapper>
+      <Styled.LectureInfoWrapper>
+        <Styled.LectureIntroduce variant="subtitle-2">
+          {lectureData.introduce}
+        </Styled.LectureIntroduce>
+        <Styled.LectureCoachName variant="body-2" textColor="gray8F">
+          {lectureData.coach.user.username}
+        </Styled.LectureCoachName>
+        <Styled.LectureTags>
+          {lectureData.tags?.map((tag) => (
+            <ContainedBadge key={tag.id} type="category">
+              {tag.content}
+            </ContainedBadge>
+          ))}
+        </Styled.LectureTags>
+      </Styled.LectureInfoWrapper>
+    </Styled.LectureCardContainer>
   );
 };
 
