e309d6598adca02647be37b10532e529d19812d0
useonglee <useong0830@naver.com>
2022-07-21 16:54:37 +0900

    #139 feat: 카테고리 아이템에 link 태그 제거 및 안드로이드 브릿지 연결

--- src/components/Home/Category/Category.tsx
+++ src/components/Home/Category/Category.tsx
@@ -1,21 +1,23 @@
-import Link from 'next/link';
 import { SvgIcon, Typograpy } from '@components/Common';
 import CATEGORY_LIST from '@constants/categoryList';
 import * as Styled from './CategoryStyle';
 
 const Category = () => {
+  const handleMoveToLectureCategoryPage = (category: string) => {
+    window.Android?.navigateToCategory(category);
+  };
+
   return (
     <Styled.CategoryList>
       {CATEGORY_LIST.map((category) => (
-        <Styled.CategoryItem key={category.name}>
-          <Link href={`/lecture-category/${category.name}`}>
-            <a>
-              <SvgIcon type={category.icon} size={36} />
-              <Typograpy variant="subtitle-4" textColor="gray6B">
-                {category.title}
-              </Typograpy>
-            </a>
-          </Link>
+        <Styled.CategoryItem
+          key={category.name}
+          onClick={() => handleMoveToLectureCategoryPage(category.name)}
+        >
+          <SvgIcon type={category.icon} size={36} />
+          <Typograpy variant="subtitle-4" textColor="gray6B">
+            {category.title}
+          </Typograpy>
         </Styled.CategoryItem>
       ))}
     </Styled.CategoryList>
