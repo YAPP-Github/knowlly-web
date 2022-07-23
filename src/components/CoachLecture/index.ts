d43aae7435b8a4318340057ca876199e567d7990
useonglee <useong0830@naver.com>
2022-07-23 16:22:19 +0900

    #112 feat: 운영클래스 도메인 관련 컴포넌트 내보내기 목록 수정

--- src/components/CoachLecture/index.ts
+++ src/components/CoachLecture/index.ts
@@ -1,4 +1,7 @@
-export { default as MatchingPopup } from './MatchingPopup/MatchingPopup';
-export { default as PlayerInfo } from './PlayerInfo/PlayerInfo';
-export { default as PlayerUser } from './PlayerUser/PlayerUser';
-export { default as PlayerSchedule } from './PlayerSchedule/PlayerSchedule';
+export { default as MatchingPopup } from './FormDetail/MatchingPopup/MatchingPopup';
+export { default as PlayerInfo } from './FormDetail/PlayerInfo/PlayerInfo';
+export { default as PlayerUser } from './FormDetail/PlayerUser/PlayerUser';
+export { default as PlayerSchedule } from './FormDetail/PlayerSchedule/PlayerSchedule';
+
+export { default as UserFormList } from './FormList/UserFormList/UserFormList';
+export { default as UserFormCard } from './FormList/UserFormCard/UserFormCard';
