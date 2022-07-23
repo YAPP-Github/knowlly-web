dd32cf74670bf5deaebc0b0cf1e4545cab775164
TaeeunKim <xodms0309@naver.com>
2022-07-23 18:54:20 +0900

    #155 feat: profile isFetching status 추가

--- src/hooks/profile/useProfile.ts
+++ src/hooks/profile/useProfile.ts
@@ -3,8 +3,13 @@ import { useQuery } from 'react-query';
 import queryKeys from '@react-query/keys';
 import { IProfile } from '@/types/profile';
 
-const useProfile = (userId: number): IProfile => {
-  const { data: userProfile } = useQuery(
+interface IUseProfile {
+  user: IProfile;
+  isFetching: boolean;
+}
+
+const useProfile = (userId: number): IUseProfile => {
+  const { data: user, isFetching } = useQuery(
     [queryKeys.userProfile, userId],
     () => api.fetchUserProfile(userId),
     {
@@ -12,7 +17,7 @@ const useProfile = (userId: number): IProfile => {
     }
   );
 
-  return userProfile;
+  return { user, isFetching };
 };
 
 export default useProfile;
