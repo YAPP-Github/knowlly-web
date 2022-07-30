cb8058de4e67a7ebd7b5c934162859fc3d6c9262
useonglee <useong0830@naver.com>
2022-07-30 12:40:50 +0900

    #170 feat: nextjs 외부 폴더 접근 허용 옵션 추가

--- packages/knowlly-webview/next.config.js
+++ packages/knowlly-webview/next.config.js
@@ -3,6 +3,9 @@ const withInterceptStdout = require('next-intercept-stdout');
 
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    externalDir: true,
+  },
   env: {
     BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
     TEST_TOKEN: process.env.NEXT_PUBLIC_TEST_TOKEN,
