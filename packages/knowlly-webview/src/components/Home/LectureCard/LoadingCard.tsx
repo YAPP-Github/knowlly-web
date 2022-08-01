38d8225fa08bd799633f2ac15199189bd429e685
Ayaan <uslee@maxst.com>
2022-08-02 00:36:47 +0900

    #194 feat: 클래스 카드 로딩 UI 부모 태그 제거

--- packages/knowlly-webview/src/components/Home/LectureCard/LoadingCard.tsx
+++ packages/knowlly-webview/src/components/Home/LectureCard/LoadingCard.tsx
@@ -2,18 +2,16 @@ import * as Styled from './LoadingCardStyle';
 
 const LoadingCard = () => {
   return (
-    <li>
-      <Styled.SkeletonCardContainer>
-        <Styled.SkeletonImageWrapper>
-          <Styled.SkeletonImage />
-        </Styled.SkeletonImageWrapper>
-        <Styled.SkeletonInfoWrapper>
-          <Styled.SkeletonIntroduce />
-          <Styled.SkeletonCoachName />
-          <Styled.SkeletonContainedBadge />
-        </Styled.SkeletonInfoWrapper>
-      </Styled.SkeletonCardContainer>
-    </li>
+    <Styled.SkeletonCardContainer>
+      <Styled.SkeletonImageWrapper>
+        <Styled.SkeletonImage />
+      </Styled.SkeletonImageWrapper>
+      <Styled.SkeletonInfoWrapper>
+        <Styled.SkeletonIntroduce />
+        <Styled.SkeletonCoachName />
+        <Styled.SkeletonContainedBadge />
+      </Styled.SkeletonInfoWrapper>
+    </Styled.SkeletonCardContainer>
   );
 };
 
