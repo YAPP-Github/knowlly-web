aaea9a8fcc712ee8ec66ee6bd3365a99287c4c57
Ayaan <uslee@maxst.com>
2022-08-02 00:37:38 +0900

    #194 feat: 카테고리 클래스 목록 ISR 렌더링 방식 제거 및 CSR 렌더링 방식으로 수정

--- packages/knowlly-webview/src/pages/lecture-category/[name].tsx
+++ packages/knowlly-webview/src/pages/lecture-category/[name].tsx
@@ -1,5 +1,5 @@
 import { Fragment, useEffect, useMemo } from 'react';
-import type { GetStaticPaths, GetStaticProps, NextPage } from 'next';
+import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
 import { Typograpy } from '@components/Common';
 import { PageLayout, Section } from '@components/Common/Layout';
@@ -7,17 +7,14 @@ import { LectureList, LoadingList } from '@components/Home';
 import useInfiniteLecture from '@hooks/home/useInfiniteLecture';
 import CATEGORY_LIST from '@constants/categoryList';
 import { useInView } from 'react-intersection-observer';
-import { dehydrate, QueryClient } from 'react-query';
-import queryKeys from '@react-query/keys';
-import api from '@api';
 
 const LectureCategoryPage: NextPage = () => {
   const router = useRouter();
-  const categoryName = String(router.query.name);
+  const categoryName = router.query.name;
 
   const { lectureInfoList, fetchNextPage, isLoading, isFetching } = useInfiniteLecture(
     '?categoryName',
-    categoryName
+    categoryName as string
   );
 
   const { ref, inView } = useInView();
@@ -54,30 +51,6 @@ const LectureCategoryPage: NextPage = () => {
       </Section>
     </PageLayout>
   );
-};
-
-export const getStaticProps: GetStaticProps = async (context) => {
-  const categoryName = context.params?.name;
-  const queryString = `?categoryName=${categoryName}&page=0&size=10`;
-  const queryClient = new QueryClient();
-
-  await queryClient.prefetchInfiniteQuery([queryKeys.lectureInfo, queryString], () =>
-    api.fetchLectureInfo(queryString)
-  );
-
-  return {
-    props: { dehydratedState: JSON.parse(JSON.stringify(dehydrate(queryClient))) },
-    revalidate: 60,
-  };
-};
-
-export const getStaticPaths: GetStaticPaths = async () => {
-  const paths = CATEGORY_LIST.map((category) => ({ params: { name: `${category.name}` } }));
-
-  return {
-    paths,
-    fallback: 'blocking',
-  };
 };
 
 export default LectureCategoryPage;
