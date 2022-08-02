ca165d194c878ac1df87b9dd8128746ceb8d158c
TaeeunKim <xodms0309@naver.com>
2022-08-02 17:28:28 +0900

    #196 feat: 개인정보 처리방침, 서비스 이용약관 url 연결

--- packages/knowlly-landing/src/components/Footer/Footer.tsx
+++ packages/knowlly-landing/src/components/Footer/Footer.tsx
@@ -12,8 +12,22 @@ const Footer = () => {
       <Styled.CopyrightsWrapper>
         <Styled.Copyrights>© 2022 knowlly. Copyright and rights reserved</Styled.Copyrights>
         <Styled.ServiceTermsWrapper>
-          <Styled.Copyrights>개인정보 처리방침</Styled.Copyrights>
-          <Styled.Copyrights>서비스 이용약관</Styled.Copyrights>
+          <Styled.Copyrights>
+            <a
+              target="_blank"
+              href="https://sunsetmood.notion.site/Knowlly-6293f0a979e64afbb220ebc5d0e1519f"
+            >
+              개인정보 처리방침
+            </a>
+          </Styled.Copyrights>
+          <Styled.Copyrights>
+            <a
+              target="_blank"
+              href="https://sunsetmood.notion.site/Knowlly-ad0dfa18936743729c240af88df170f0"
+            >
+              서비스 이용약관
+            </a>
+          </Styled.Copyrights>
         </Styled.ServiceTermsWrapper>
       </Styled.CopyrightsWrapper>
     </Styled.FooterContainer>
