5908f2084937fd565f7120f55032aed3569522f3
TaeeunKim <xodms0309@naver.com>
2022-08-02 15:55:52 +0900

    #196 feat: Footer contents 작성

--- packages/knowlly-landing/src/components/Footer/Footer.tsx
+++ packages/knowlly-landing/src/components/Footer/Footer.tsx
@@ -2,7 +2,22 @@ import React from 'react';
 import * as Styled from './FooterStyle';
 
 const Footer = () => {
-  return <Styled.FooterContainer>footer</Styled.FooterContainer>;
+  return (
+    <Styled.FooterContainer>
+      <Styled.IntroduceTextWrapper>
+        <Styled.KnowllyText>KNOWLLY (널리)</Styled.KnowllyText>
+        <Styled.Description>Get started new try our product</Styled.Description>
+        <Styled.Description>Email: knowlly2022@gmail.com</Styled.Description>
+      </Styled.IntroduceTextWrapper>
+      <Styled.CopyrightsWrapper>
+        <Styled.Copyrights>© 2022 knowlly. Copyright and rights reserved</Styled.Copyrights>
+        <Styled.ServiceTermsWrapper>
+          <Styled.Copyrights>개인정보 처리방침</Styled.Copyrights>
+          <Styled.Copyrights>서비스 이용약관</Styled.Copyrights>
+        </Styled.ServiceTermsWrapper>
+      </Styled.CopyrightsWrapper>
+    </Styled.FooterContainer>
+  );
 };
 
 export default Footer;
--- packages/knowlly-landing/src/components/Footer/FooterStyle.ts
+++ packages/knowlly-landing/src/components/Footer/FooterStyle.ts
@@ -5,3 +5,53 @@ export const FooterContainer = styled.div`
   height: 368px;
   background-color: ${theme.color.gray['EF']};
 `;
+
+export const IntroduceTextWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 34.8rem;
+  padding-top: 9.9rem;
+  & > * {
+    margin-bottom: 1.6rem;
+  }
+`;
+
+export const KnowllyText = styled.span`
+  font-family: 'SUIT';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 2.2rem;
+  color: ${theme.color.primary['basic']};
+`;
+
+export const Description = styled.span`
+  font-family: 'SUIT';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 1.6rem;
+  color: ${theme.color.gray['44']};
+`;
+
+export const CopyrightsWrapper = styled.div`
+  position: absolute;
+  display: flex;
+  right: 30rem;
+
+  & > :first-child {
+    margin-right: 11.4rem;
+  }
+`;
+
+export const Copyrights = styled.p`
+  font-family: 'SUIT';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 18px;
+  color: ${theme.color.gray['6B']};
+`;
+
+export const ServiceTermsWrapper = styled.div`
+  & > :first-child {
+    margin-bottom: 1rem;
+  }
+`;
