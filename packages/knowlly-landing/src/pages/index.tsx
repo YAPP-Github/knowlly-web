c42716b9fdf20341a13902bbf2ff1b2b218a9145
Ayaan <uslee@maxst.com>
2022-08-05 14:09:42 +0900

    #210 feat: 메인페이지에  소개 컴포넌트 추가

--- packages/knowlly-landing/src/pages/index.tsx
+++ packages/knowlly-landing/src/pages/index.tsx
@@ -1,17 +1,20 @@
+import type { NextPage } from 'next';
+import Introduce from '@components/Introduce';
 import Footer from '@components/Footer/Footer';
 import Feature from '@components/Feature/Feature';
 import Guide from '@components/Guide/Guide';
 import Download from '@components/Download/Download';
-import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
-    <>
+    <main>
+      <Introduce.App />
+      <Introduce.Service />
       <Feature />
       <Guide />
       <Download />
       <Footer />
-    </>
+    </main>
   );
 };
 
